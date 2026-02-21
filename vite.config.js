32690fedf5789e2e73c8fb8c605a8a165617bb9b
madhumitha0511 <154619550+madhumitha0511@users.noreply.github.com>
2026-02-21 14:15:03 +0530

    up

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,15 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    outDir: 'dist',
+  },
+  preview: {
+    port: process.env.PORT ? parseInt(process.env.PORT) : 4173,
+    host: '0.0.0.0',
+  },
+  server: {
+    port: 3000,
+    host: true,
+  },
 })
